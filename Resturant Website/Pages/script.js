27851db0b7bf46f34b1649edc6c9dacbe6876dbd
Prince <princegoutam65101@gmail.com>
2024-07-21 08:49:05 +0530

    updated

--- Resturant Website/Pages/script.js
+++ Resturant Website/Pages/script.js
@@ -1,46 +1,46 @@
-const btns = document.querySelectorAll(".menu-buttons button");
-const imgs = document.querySelectorAll(".item img");
+// const btns = document.querySelectorAll(".menu-buttons button");
+// const imgs = document.querySelectorAll(".item img");
 
-for (let i = 1; i < btns.length; i++) {
-    btns[i].addEventListener("click", filterImg);
+// for (let i = 1; i < btns.length; i++) {
+//     btns[i].addEventListener("click", filterImg);
 
-}
+// }
 
-function setActiveBtn(e) {
-    btns.forEach(btn => {
-        btn.classList.remove("btn-clicked");
-    })
+// function setActiveBtn(e) {
+//     btns.forEach(btn => {
+//         btn.classList.remove("btn-clicked");
+//     })
 
-    e.target.classList.add("btn-clicked");
-}
+//     e.target.classList.add("btn-clicked");
+// }
 
-function filterImg(e) {
-    setActiveBtn(e);
+// function filterImg(e) {
+//     setActiveBtn(e);
 
-    imgs.forEach(img => {
-        img.classList.remove('img-shrink');
-        img.classList.add('img-expand');
-
-
+//     imgs.forEach(img => {
+//         img.classList.remove('img-shrink');
+//         img.classList.add('img-expand');
 
 
-        const imgType = parseInt(img.dataset.img);
 
-        const btnType = parseInt(e.target.dataset.btn);
 
-        if (imgType !== btnType) {
-            img.classList.add('img-shrink');
-            img.classList.remove('img-expand');
-        }
-    })
-}
+//         const imgType = parseInt(img.dataset.img);
 
-btns[0].addEventListener('click', (e) => {
-    setActiveBtn(e);
+//         const btnType = parseInt(e.target.dataset.btn);
 
-    imgs.forEach(img => {
-        img.classList.add('img-expanded');
-        img.classList.remove('img-shrink');
-    })
+//         if (imgType !== btnType) {
+//             img.classList.add('img-shrink');
+//             img.classList.remove('img-expand');
+//         }
+//     })
+// }
 
-})
+// btns[0].addEventListener('click', (e) => {
+//     setActiveBtn(e);
+
+//     imgs.forEach(img => {
+//         img.classList.add('img-expanded');
+//         img.classList.remove('img-shrink');
+//     })
+
+// })
