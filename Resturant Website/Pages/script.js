e3b8178a82f2b5e4314fa78d9754b5dfdb840dc1
Prince <princegoutam65101@gmail.com>
2024-07-24 10:46:21 +0530

    updated

--- Resturant Website/Pages/script.js
+++ Resturant Website/Pages/script.js
@@ -2,6 +2,7 @@ const loginDialog = document.getElementById("loginDialog");
 const headText = document.querySelector(".head-text");
 const button = document.getElementById("login");
 
+
 loginDialog.addEventListener("click",()=>{
     document.body.classList.add("openDialog");
     document.body.classList.add("hide");
@@ -10,5 +11,29 @@ loginDialog.addEventListener("click",()=>{
 button.addEventListener("click",()=>{
     document.body.classList.add("hideDialog");
     document.body.classList.remove("hide");
+})
+
+
+// Slider  Part 
+
+let scrollContainer = document.querySelector(".fav-item-gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel",(evt)=>{
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+});
+
+nextBtn.addEventListener("click",()=>{
+    scrollContainer.scrollLeft += 300;
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.style.transtion="0.5s";
+});
+
+backBtn.addEventListener("click",()=>{
+    scrollContainer.scrollLeft -= 300;
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.style.transtion="0.5s";
 })
 
